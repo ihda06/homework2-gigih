33005b0dfc1def70bc34cc4221c81d8cb7181dc6
ihda06 <ihdaanwari5@gmail.com>
2022-03-21 17:42:26 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const spotify_secret_key = process.env.REACT_APP_SPOTIFY_KEY;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
